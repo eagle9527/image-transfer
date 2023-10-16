b8e944525378a5b95a066582c1bdebf4cc2cbfbf
eagle9527 <eagle@qq.com>
2023-10-16 14:13:17 +0800

    禁止证书验证

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ go run cmd/image-transfer/main.go  \
   --routines=20  \
   --securityFile=./registry-secret.yaml  \
    --ruleFile=transfer-rule.yaml   \
-   --ns=lunz-devops  \
+   --ns=xxxxxxxxxx  \
    --registry=swr.cn-east-3.myhuaweicloud.com   \
    --retry=3  \
    --qps=100
