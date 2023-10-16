020857436d58b017a182d6ca159930b26937cd57
eagle9527 <eagle@qq.com>
2023-10-16 14:14:43 +0800

    禁止证书验证

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 go run cmd/image-transfer/main.go  \
   --routines=20  \
   --securityFile=./registry-secret.yaml  \
-   --ruleFile=transfer-rule.yaml   \
-   --ns=xxxxxxxxxx  \
-   --registry=swr.cn-east-3.myhuaweicloud.com   \
-   --retry=3  \
-   --qps=100
+  --ruleFile=transfer-rule.yaml   \
+  --ns=xxxxxxxxxx  \
+  --registry=swr.cn-east-3.myhuaweicloud.com   \
+  --retry=3  \
+  --qps=100
